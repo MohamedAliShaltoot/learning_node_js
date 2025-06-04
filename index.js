176f23b69698ed4f46a56fafd0ea98a4ab12575a
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-06-04 19:33:01 +0300

    function in js

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-var age = [
-  
-  {
-    name: "Jane",
-    age: 25,
-  },
-  {
-    name: "Bob",
-    age: 35,
-  },
-  {
-    name: "Alice",
-    age: 28,
-  },
-  {
-    name: "Charlie",
-    age: 32,
-  },
-];
-console.log(age[3].name);
+function calculateSalary(userName,salary,profits,taxis){
+var total = (salary + profits - taxis);
+   // return (salary + profits - taxis).toFixed(2);
+console.log(userName + " has a total salary of " + total);
 
+}
 
+calculateSalary("John", 1000, 0, 0);
