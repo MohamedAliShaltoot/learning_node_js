9ce4ff15912ee1d5d55bb3725a6f69df31d0fe60
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-06-04 19:12:37 +0300

    two files one for pratise other for notes

---
+++ index.js
@@ -0,0 +1,22 @@
+var age = [
+  
+  {
+    name: "Jane",
+    age: 25,
+  },
+  {
+    name: "Bob",
+    age: 35,
+  },
+  {
+    name: "Alice",
+    age: 28,
+  },
+  {
+    name: "Charlie",
+    age: 32,
+  },
+];
+console.log(age[3].name);
+
+
---
+++ notes.md
@@ -0,0 +1,8 @@
+## 1] === VS ==
+
+=== compares between values only
+== compares between types
+
+## 2] False values :
+
+    a) false b)""  c) 0   d) null d) undefined
