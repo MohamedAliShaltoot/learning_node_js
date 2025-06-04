b6134195bb0e95f561c36325e7700f6e8244b808
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-06-04 20:36:01 +0300

    apply on Expression Function

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-function calculateSalary(userName,salary,profits,taxis){
-var total = (salary + profits - taxis);
-   // return (salary + profits - taxis).toFixed(2);
-console.log(userName + " has a total salary of " + total);
+// function calculateSalary(userName,salary,profits,taxis){
+// var total = (salary + profits - taxis);
+//    // return (salary + profits - taxis).toFixed(2);
+// console.log(userName + " has a total salary of " + total);
 
+// }
+
+// calculateSalary("John", 1000, 0, 0);
+
+var demo = function (printedDemo) {
+    console.log(printedDemo);
 }
-
-calculateSalary("John", 1000, 0, 0);
+demo("demo");
