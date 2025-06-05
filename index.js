cf8d06b99e367eec60decc199ed047c5510d2c7b
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-06-06 00:22:22 +0300

    create first api

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const http = module.require("http");
+var users = [
+    { id: 1, name: "John", age: 30 },
+    { id: 2, name: "Jane", age: 25 },
+    { id: 3, name: "Bob" , age: 35},
+];
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Content-Type", "application/json");
   res.end(
-    
+    JSON.stringify(users)
   );
 });
 server.listen(3000, () => console.log("listening on port 3000")); // every application must have a server listening on a port
