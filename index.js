8273557bd0122f105099e8e71236d863e2e2433b
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-06-06 00:17:42 +0300

    how to create a server and print msgs in it as localhost:3000

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-// function calculateSalary(userName,salary,profits,taxis){
-// var total = (salary + profits - taxis);
-//    // return (salary + profits - taxis).toFixed(2);
-// console.log(userName + " has a total salary of " + total);
-
-// }
-
-// calculateSalary("John", 1000, 0, 0);
-
-var user={
-    name: "mo",
-    age:30,
-    children:{
-        child1:{
-            name:"omr"
-        },
-         child2:{
-            name:"ahmed"
-        }
-    }
-}
-
-
-
-let {name,age,children} = user  
-console.log(name,age,children);
-console.log( children.child1.name);
-console.log(user.children.child1.name);
+const http = module.require("http");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end(
+    
+  );
+});
+server.listen(3000, () => console.log("listening on port 3000")); // every application must have a server listening on a port
